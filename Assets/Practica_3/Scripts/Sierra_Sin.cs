52fa9038e8cba56546e7086bf19cf3446a5eea4f
Juan Valdes <mw3_america@hotmail.com>
2019-06-10 11:29:06 -0500

    Initial commit

---
+++ Assets/Practica_3/Scripts/Sierra_Sin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Nombre de Desarrollador: Juan Carlos Valdes Aguilar    
+//Materia: Estructura de datos     
+//Fecha: 03-Junio-19     
+//Practica: 3
+public class Sierra_Sin : MonoBehaviour
+{
+    private Vector3 originalTransform;
+
+    public float velocidad;
+    public float distancia;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalTransform = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = originalTransform + new Vector3(0, Mathf.Sin(Time.time * velocidad) * distancia, 0);
+    }
+}
---
+++ Assets/Scripts/Movimiento_2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_2 : MonoBehaviour
+{
+    #region Variables
+    float velocidad = 3f;
+    public float tiempo = 0;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += 0.5f * Time.deltaTime;
+
+        if (tiempo >= 5.0f)
+        {
+            transform.Translate(Vector3.forward * velocidad);
+        }
+    }
+}
+//Fin de la clase de Movimiento
