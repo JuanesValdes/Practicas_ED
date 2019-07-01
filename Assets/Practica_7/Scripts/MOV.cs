8612926ed2a4243f51ae8ecadf6f76a4f9d75003
Juan Valdes <mw3_america@hotmail.com>
2019-07-01 16:08:28 -0500

    P7
    Creación de misiles y su ejecución de disparo al igual que la bala del tanque

---
+++ Assets/Practica_7/Scripts/MOV.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocidad=5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * velocidad * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * velocidad * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Practica_7/Scripts/TorretaD.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum TorretaIA
+{
+    Buscando,
+    Ataque
+}
+
+public class TorretaD : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public Transform[] cañon;
+    public float fuerzaDisparo;
+    public TorretaIA estadoActual = new TorretaIA();
+    int ContadorMisiles;
+
+    void Start ()
+    {
+        ContadorMisiles = 0;
+        estadoActual = TorretaIA.Buscando;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (estadoActual==TorretaIA.Ataque)
+        {
+            for (int i =0; i < 3; i++)
+            {
+                Rigidbody bulletPos = Instantiate(bullet, cañon[i].position, cañon[i].rotation) as Rigidbody;
+                bulletPos.AddForce(cañon[i].forward * fuerzaDisparo);
+                ContadorMisiles = i+1;
+
+                if (ContadorMisiles >= 3 && i >= cañon.Length-1)
+                {
+                    ContadorMisiles = 3;
+                    estadoActual = TorretaIA.Buscando;
+                }
+            }
+          
+        }
+
+    }
+}
+
